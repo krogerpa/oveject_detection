cff8e85ad320d4bbb7bbbfa8b156b5ae8e7b3028
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-06-08 12:38:41 +0200

    crop images from annotations

---
+++ Multiclass_Object_Classification-master/AIBallAnnotationParser.py
@@ -0,0 +1,66 @@
+import os
+
+
+class AIBallAnnotationParser:
+
+    def __init__(self, dir_path):
+        self.export_file_id = "export"
+        sub_dir_list = os.listdir(dir_path)
+
+        for item in sub_dir_list:
+            if self.export_file_id in item:
+                self.file_path = os.path.join(dir_path, item)
+                print("found annotations file: {}".format(self.file_path))
+                break
+
+        assert self.file_path is not None
+
+    def read_ai_ball_file(self):
+        annotations = dict()
+
+        with open(self.file_path, 'r') as fp:
+            lines = fp.read().splitlines()
+
+            format_columns = None
+
+            for i, line in enumerate(lines):
+
+                if '[format: "' in line:
+                    format_columns = self.parse_format(line)
+                    continue
+
+                if format_columns is None:
+                    continue
+
+                if line is "":
+                    continue
+
+                filename, annotation = self.parse_annotation(format_columns, line)
+
+                if annotation is not None:
+                    annotations[filename] = annotation
+
+        return annotations
+
+    def parse_format(self, line):
+        tmp1 = line.split('[format: "')
+        tmp2 = tmp1[1].split('"]')
+        columns = tmp2[0].split('|')
+        print('found columns: {}'.format(columns))
+        return columns
+
+    def parse_annotation(self, format_columns, line):
+        tokens = line.split('|')
+
+        annotation = dict()
+
+        if len(tokens) != len(format_columns):
+            assert 'not_in_image' in line
+            filename_index = format_columns.index("filename")
+            annotation['filename'] = tokens[filename_index]
+            return annotation['filename'], annotation
+
+        for i, column in enumerate(format_columns):
+            annotation[column] = tokens[i]
+
+        return annotation['filename'], annotation
--- Multiclass_Object_Classification-master/DataConverter.py
+++ Multiclass_Object_Classification-master/DataConverter.py
@@ -1,25 +1,53 @@
+import os
 
 import numpy as np
-import os
-from tensorflow import keras
+from AIBallAnnotationParser import *
 from PIL import Image
 from sklearn.model_selection import train_test_split
+from tensorflow import keras
 
 dimen = 32
 
-dir_path = input( 'Enter images directory path : ')
-output_path = input( 'Enter output directory path : ')
+dir_path = input('Enter images directory path : ')
+output_path = input('Enter output directory path : ')
 
-sub_dir_list = os.listdir( dir_path )
+sub_dir_list = os.listdir(dir_path)
 images = list()
 labels = list()
-for i in range( len( sub_dir_list ) ):
+for i, sub_dir_name in enumerate(sub_dir_list):
     label = i
-    image_names = os.listdir( dir_path + sub_dir_list[i] )
+    sub_dir_path = os.path.join(dir_path, sub_dir_name)
+
+    if not os.path.isdir(sub_dir_path):
+        continue
+
+    parser = AIBallAnnotationParser(sub_dir_path)
+    annotations = parser.read_ai_ball_file()
+    print('subdir: "{}" label: "{}" found {} annotations: {}'.format(sub_dir_name, i, len(annotations), annotations))
+
+    image_names = os.listdir(sub_dir_path)
     for image_path in image_names:
-        path = dir_path + sub_dir_list[i] + "/" + image_path
-        image = Image.open( path ).convert( 'L' )
-        resize_image = image.resize((dimen, dimen))
+        path = os.path.join(sub_dir_path, image_path)
+
+        if os.path.splitext(path)[1] == '.txt' or image_path == '.DS_Store':
+            continue
+
+        image = Image.open(path).convert('L')
+
+        annotation = annotations[image_path]
+        if len(annotation) == 1:
+            print('skip image: {}', image_path)
+            continue
+        crop_area = (int(annotation['x1']), int(annotation['y1']), int(annotation['x2']), int(annotation['y2']))
+        cropped_img = image.crop(crop_area)
+
+        resize_image = cropped_img.resize((dimen, dimen))
+
+        # image.show()
+        # cropped_img.show()
+        # resize_image.show()
+        # exit(9)
+
         array = list()
         for x in range(dimen):
             sub_array = list()
@@ -29,16 +57,14 @@ for i in range( len( sub_dir_list ) ):
         image_data = np.array(array)
         image = np.array(np.reshape(image_data, (dimen, dimen, 1))) / 255
         images.append(image)
-        labels.append( label )
+        labels.append(label)
 
-x = np.array( images )
-y = np.array( keras.utils.to_categorical( np.array( labels) , num_classes=len(sub_dir_list) ) )
+x = np.array(images)
+y = np.array(keras.utils.to_categorical(np.array(labels), num_classes=len(sub_dir_list)))
 
-train_features , test_features ,train_labels, test_labels = train_test_split( x , y , test_size=0.4 )
+train_features, test_features, train_labels, test_labels = train_test_split(x, y, test_size=0.4)
 
-np.save( '{}x.npy'.format( output_path )  , train_features )
-np.save( '{}y.npy'.format( output_path )  , train_labels )
-np.save( '{}test_x.npy'.format( output_path ) , test_features )
-np.save( '{}test_y.npy'.format( output_path ) , test_labels )
-
-
+np.save('{}x.npy'.format(output_path), train_features)
+np.save('{}y.npy'.format(output_path), train_labels)
+np.save('{}test_x.npy'.format(output_path), test_features)
+np.save('{}test_y.npy'.format(output_path), test_labels)
