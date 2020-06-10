3d93162b5f0cbff5da80059685efc1351ed611b1
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-06-10 08:55:39 +0200

    refactor file

--- tutorials/Multiclass_Object_Classification-master/MainFile.py
+++ tutorials/Multiclass_Object_Classification-master/MainFile.py
@@ -1,4 +1,3 @@
-
 from tensorflow.python.keras.callbacks import TensorBoard
 from Model import Classifier
 import numpy as np
@@ -6,38 +5,37 @@ import time
 
 data_dimension = 32
 
-X = np.load( 'test1_images/x.npy'.format( data_dimension ))
-Y = np.load( 'test1_images/y.npy'.format( data_dimension ))
-test_X = np.load( 'test1_images/test_x.npy'.format( data_dimension ))
-test_Y = np.load( 'test1_images/test_y.npy'.format( data_dimension ))
+X = np.load('test1_images/x.npy'.format(data_dimension))
+Y = np.load('test1_images/y.npy'.format(data_dimension))
+test_X = np.load('test1_images/test_x.npy'.format(data_dimension))
+test_Y = np.load('test1_images/test_y.npy'.format(data_dimension))
 
-print( X.shape )
-print( Y.shape )
-print( test_X.shape )
-print( test_Y.shape )
+print(X.shape)
+print(Y.shape)
+print(test_X.shape)
+print(test_Y.shape)
 
-X = X.reshape( ( X.shape[0] , data_dimension**2  ) ).astype( np.float32 )
-test_X = test_X.reshape( ( test_X.shape[0] , data_dimension**2 ) ).astype( np.float32 )
+X = X.reshape((X.shape[0], data_dimension ** 2)).astype(np.float32)
+test_X = test_X.reshape((test_X.shape[0], data_dimension ** 2)).astype(np.float32)
 
-classifier = Classifier( number_of_classes=8 )
-classifier.load_model( 'models/model.h5')
+print(X.shape)
+
+classifier = Classifier(number_of_classes=1)
+classifier.load_model('models/model.h5')
 
 parameters = {
-    'batch_size' : 250 ,
-    'epochs' : 10 ,
-    'callbacks' : None ,
-    'val_data' : None
+    'batch_size': 50,
+    'epochs': 10,
+    'callbacks': None,
+    'val_data': None
 }
 
-classifier.fit( X , Y  , hyperparameters=parameters )
-classifier.save_model( 'models/model.h5')
+classifier.fit(X, Y, hyperparameters=parameters)
+classifier.save_model('models/model.h5')
 
-loss , accuracy = classifier.evaluate( test_X , test_Y )
-print( "Loss of {}".format( loss ) , "Accuracy of {} %".format( accuracy * 100 ) )
+loss, accuracy = classifier.evaluate(test_X, test_Y)
+print("Loss of {}".format(loss), "Accuracy of {} %".format(accuracy * 100))
 
-sample_X = classifier.prepare_images_from_dir( 'random_images/' )
-sample_X = sample_X.reshape( ( sample_X.shape[0] , data_dimension**2 ) ).astype( np.float32 )
-print( classifier.predict( sample_X ).argmax( 1 ) )
-
-
-
+sample_X = classifier.prepare_images_from_dir('random_images/')
+sample_X = sample_X.reshape((sample_X.shape[0], data_dimension ** 2)).astype(np.float32)
+print(classifier.predict(sample_X).argmax(1))
